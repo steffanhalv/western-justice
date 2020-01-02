6f9a96547f54faf6a47ac4707f4e07481375d857
steffanhalv <steffanhalv@gmail.com>
2020-01-02 19:37:03 +0100

    Created login

---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="login">
+      <h1 style="width: 100%">Western Justice</h1>
+      <input v-model="username" class="username">
+      <input v-model="password" type="password" class="password">
+      <button @click="login">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.login {
+  margin: 100px auto;
+  width: 500px;
+  font-size: 25px;
+  text-align: center;
+}
+.login input, .login button {
+  height: 50px;
+  width: 100%;
+  border: none;
+  background-color: rgb(134, 91, 56);
+  color: white;
+  margin-bottom: 10px;
+}
+</style>
